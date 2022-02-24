1db6a63bc7d012d326af886a7c28e56358e1839e
AISK11 <patrik.rajtak@gmail.com>
2022-02-24 22:35:18 +0100

    Working on text header CSS

--- src/nodejs/routes.js
+++ src/nodejs/routes.js
@@ -64,6 +64,52 @@ router.all("/notes", (req, res) => {
   res.render(`${PublicDir}/ejs/article.ejs`, { "template": template });
 });
 
+/**************************************/
+/************** linuces ***************/
+/**************************************/
+/*********************************** gentoo ***********************************/
+router.all("/notes/linuces/gentoo*", (req, res, next) => {
+  /* set data accordingly for website template */
+  template.header.img = "/svg/gentoo.svg";
+  template.header.href = "/notes/linuces/gentoo";
+  /* execute next matched express server function */
+  next();
+});
+
+router.all("/notes/linuces/gentoo", (req, res) => {
+  /* set data accordingly for website template */
+  template.title = "Gentoo";
+  template.view = `${PublicDir}/views/notes/linuces/gentoo/gentoo.html`;
+  /* load website template with data */
+  res.render(`${PublicDir}/ejs/article.ejs`, { "template": template });
+});
+
+/************ installation ************/
+
+router.all("/notes/linuces/gentoo/installation/download", (req, res) => {
+  /* set data accordingly for website template */
+  template.title = "Gentoo | Download";
+  template.view = `${PublicDir}/views/notes/linuces/gentoo/installation/download.html`;
+  /* load website template with data */
+  res.render(`${PublicDir}/ejs/article.ejs`, { "template": template });
+});
+
+router.all("/notes/linuces/gentoo/installation/pre_installation", (req, res) => {
+  /* set data accordingly for website template */
+  template.title = "Gentoo | Pre-installation";
+  template.view = `${PublicDir}/views/notes/linuces/gentoo/installation/pre_installation.html`;
+  /* load website template with data */
+  res.render(`${PublicDir}/ejs/article.ejs`, { "template": template });
+});
+
+router.all("/notes/linuces/gentoo/installation/installation", (req, res) => {
+  /* set data accordingly for website template */
+  template.title = "Gentoo | Installation";
+  template.view = `${PublicDir}/views/notes/linuces/gentoo/installation/installation.html`;
+  /* load website template with data */
+  res.render(`${PublicDir}/ejs/article.ejs`, { "template": template });
+});
+
 /*******************************************************************************
 **                                   Tools                                    **
 *******************************************************************************/
